c7938910854f87c52a80fe3671129d8aaea5d843
ragtz <agrobomaniac@gmail.com>
2014-11-10 22:37:13 -0500

    Add pooling starter code

---
+++ layer.py
@@ -0,0 +1,16 @@
+
+class Layer:
+    def __init__(self, num_maps, h, w):
+        pass
+        
+    def init_weights(self, weights):
+        pass
+        
+    def forward_prop(self, inp):
+        pass
+        
+    def backward_prop(self, inp):
+        pass
+        
+    def update(self, w_grad, b_grad):
+        pass
