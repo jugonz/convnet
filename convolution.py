37a761c1d4af1080bff854fae94f53350af102ad
Julian Gonzalez <jugonz97@mit.edu>
2014-11-12 20:48:20 -0500

    convolution layer starting code

---
+++ convolution.py
@@ -0,0 +1,17 @@
+from layer import Layer
+
+class ConvolutionLayer(Layer):
+    def __init__(self, num_maps, h, w):
+        pass
+
+    def init_weights(self, weights):
+        pass
+
+    def forward_prop(self, inp):
+        pass
+
+    def backward_prop(self, inp):
+        pass
+
+    def update(self, w_grad, b_grad):
+        pass
