ef5cfd747d675b2978fe6153c863a1d2281f0d72
Julian Gonzalez <jugonz97@mit.edu>
2014-11-12 23:55:55 -0500

    first part of convolutional layer

--- convolution.py
+++ convolution.py
@@ -1,14 +1,51 @@
 from layer import Layer
+import numpy as np
+import scipy.signal as signal
 
+# What do we need to perform forward propagation?
+#
+# filterDim - N in (N x N) dimensions of filters
+# numFilters - number of filters
+# input - input to convolve (can get dimensions from numpy arrays)
+#
+# We generate our own W (filters) and B (biases)
 class ConvolutionLayer(Layer):
-    def __init__(self, num_maps, h, w):
-        pass
+    def __init__(self, numFilters, filterDim):
+        self.numFilters = numFilters
+        self.filterDim = filterDim
 
-    def init_weights(self, weights):
-        pass
+        # need to initialize weights... one filterDim x filterDim per filter
+        # in numpy indexing works differently than matlab, so store
+        # number of filter FIRST
+        self.W = numpy.random.randn(numFilters, filterDim, filterDim)
+        # biases: one for each filter
+        self.B = numpy.zeros(numFilters)
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-1 * x))
 
     def forward_prop(self, inp):
-        pass
+        # for now, assume inp is np datatype
+        # and size is accessible via .shape attribute
+        # and size is NumberOfImages x N x N (square input)
+        numImages = inp.shape[0]
+        convSize = inp.shape[1] - filterDim + 1
+
+        convolved = np.zeros(numImages, self.numFilters, convSize, convSize)
+        # unoptimized
+        for i in xrange(numImages):
+            for j in xrange(self.numFilters):
+                # instead of doing the 2-d convolution with the flipped filter,
+                # do the 2-d cross-correlation to skip flipping
+                image = inp[i]
+                filt = self.W[j]
+
+                output = self._sigmoid(signal.correlate2d(image, filt, "valid"))
+
+                # now, save the output
+                convolved[i, j] = output
+
+        return convolved
 
     def backward_prop(self, inp):
         pass
