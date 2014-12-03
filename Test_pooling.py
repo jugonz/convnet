dbe39f1e1343d23f4f6152d60846b34e708ff755
ragtz <agrobomaniac@gmail.com>
2014-12-03 14:15:49 -0500

    Add pooling transformations for fully connected layer

--- Test_pooling.py
+++ Test_pooling.py
@@ -48,11 +48,17 @@ class PoolingLayerTest(unittest.TestCase):
                                    [6, 5, 4],
                                    [3, 2, 1]]])
                         
-        self.max_pool4x4 = PoolingLayer(4,2,'max')
-        self.mean_pool4x4 = PoolingLayer(4,2,'mean')                
+        self.max_pool4x4 = PoolingLayer(2,4,2,'conv','max')
+        self.mean_pool4x4 = PoolingLayer(2,4,2,'conv','mean')                
                         
-        self.max_pool9x9 = PoolingLayer(9,3,'max')
-        self.mean_pool9x9 = PoolingLayer(9,3,'mean')
+        self.max_pool9x9 = PoolingLayer(2,9,3,'conv','max')
+        self.mean_pool9x9 = PoolingLayer(2,9,3,'conv','mean')
+        
+        self.max_pool4x4_full = PoolingLayer(2,4,2,'full','max')
+        self.mean_pool4x4_full = PoolingLayer(2,4,2,'full','mean')
+                        
+        self.max_pool9x9_full = PoolingLayer(2,9,3,'full','max')
+        self.mean_pool9x9_full = PoolingLayer(2,9,3,'full','mean')
         
     def test_max_forward_prop_4x4(self):
         pool = self.max_pool4x4.forward_prop(self.maps4x4)
@@ -148,6 +154,102 @@ class PoolingLayerTest(unittest.TestCase):
                  
         self.mean_pool9x9.forward_prop(self.maps9x9)       
         back = self.mean_pool9x9.backward_prop(self.error3x3)
+        self.assertTrue(np.allclose(back, ans))
+        
+    def test_max_forward_prop_4x4_full(self):
+        pool = self.max_pool4x4_full.forward_prop(self.maps4x4)
+        self.assertTrue((pool[0] == np.array([4*np.ones((2,2)), 8*np.ones((2,2))]).flatten()).all())
+        
+    def test_max_backward_prop_4x4_full(self):
+        ans = np.array([[[1, 0, 0, 2],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [3, 0, 0, 4]],
+                          
+                        [[4, 0, 0, 3],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [2, 0, 0, 1]]])
+                    
+        self.max_pool4x4_full.forward_prop(self.maps4x4)     
+        back = self.max_pool4x4_full.backward_prop(self.error2x2.flatten())
+        self.assertTrue((back == ans).all())
+        
+    def test_mean_forward_prop_4x4_full(self):
+        pool = self.mean_pool4x4_full.forward_prop(self.maps4x4)
+        self.assertTrue((pool == np.array([np.ones((2,2)), 2*np.ones((2,2))]).flatten()).all())
+       
+    def test_mean_backward_prop_4x4_full(self):
+        ans = np.array([[[1., 1., 2., 2.],
+                         [1., 1., 2., 2.],
+                         [3., 3., 4., 4.],
+                         [3., 3., 4., 4.]],
+                          
+                        [[4., 4., 3., 3.],
+                         [4., 4., 3., 3.],
+                         [2., 2., 1., 1.],
+                         [2., 2., 1., 1.]]])/4
+                         
+        self.mean_pool4x4_full.forward_prop(self.maps4x4)
+        back = self.mean_pool4x4_full.backward_prop(self.error2x2.flatten())
+        self.assertTrue((back == ans).all())    
+        
+    def test_max_forward_prop_9x9_full(self):
+        pool = self.max_pool9x9_full.forward_prop(self.maps9x9)
+        self.assertTrue((pool[0] == np.array([9*np.ones((3,3)), 18*np.ones((3,3))]).flatten()).all())
+        
+    def test_max_backward_prop_9x9_full(self):
+        ans = np.array([[[1, 0, 0, 0, 2, 0, 0, 0, 3],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [4, 0, 0, 0, 5, 0, 0, 0, 6],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [7, 0, 0, 0, 8, 0, 0, 0, 9]],
+                         
+                        [[9, 0, 0, 0, 8, 0, 0, 0, 7],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [6, 0, 0, 0, 5, 0, 0, 0, 4],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         [3, 0, 0, 0, 2, 0, 0, 0, 1]]])
+               
+        self.max_pool9x9_full.forward_prop(self.maps9x9)          
+        back = self.max_pool9x9_full.backward_prop(self.error3x3.flatten())
+        self.assertTrue((back == ans).all())
+        
+    def test_mean_forward_prop_9x9_full(self):
+        pool = self.mean_pool9x9_full.forward_prop(self.maps9x9)
+        self.assertTrue((pool == np.array([np.ones((3,3)), 2*np.ones((3,3))]).flatten()).all())
+        
+    def test_mean_backward_prop_9x9_full(self):
+        ans = np.array([[[1., 1., 1., 2., 2., 2., 3., 3., 3.],
+                         [1., 1., 1., 2., 2., 2., 3., 3., 3.],
+                         [1., 1., 1., 2., 2., 2., 3., 3., 3.],
+                         [4., 4., 4., 5., 5., 5., 6., 6., 6.],
+                         [4., 4., 4., 5., 5., 5., 6., 6., 6.],
+                         [4., 4., 4., 5., 5., 5., 6., 6., 6.], 
+                         [7., 7., 7., 8., 8., 8., 9., 9., 9.],
+                         [7., 7., 7., 8., 8., 8., 9., 9., 9.],
+                         [7., 7., 7., 8., 8., 8., 9., 9., 9.]],
+                          
+                        [[9., 9., 9., 8., 8., 8., 7., 7., 7.],
+                         [9., 9., 9., 8., 8., 8., 7., 7., 7.],
+                         [9., 9., 9., 8., 8., 8., 7., 7., 7.],
+                         [6., 6., 6., 5., 5., 5., 4., 4., 4.],
+                         [6., 6., 6., 5., 5., 5., 4., 4., 4.],
+                         [6., 6., 6., 5., 5., 5., 4., 4., 4.], 
+                         [3., 3., 3., 2., 2., 2., 1., 1., 1.],
+                         [3., 3., 3., 2., 2., 2., 1., 1., 1.],
+                         [3., 3., 3., 2., 2., 2., 1., 1., 1.]]])/9
+                 
+        self.mean_pool9x9_full.forward_prop(self.maps9x9)       
+        back = self.mean_pool9x9_full.backward_prop(self.error3x3.flatten())
         self.assertTrue(np.allclose(back, ans))
     
 if __name__ == "__main__":
--- pooling.py
+++ pooling.py
@@ -2,14 +2,25 @@ from layer import Layer
 import numpy as np
 
 class PoolingLayer(Layer):
-    def __init__(self, mapSize, winSize, type='max'):
+    def __init__(self, numMaps, mapSize, winSize, nextLayer='conv', type='max'):
+        # check integer number of pooling windows
+        assert(mapSize%winSize == 0)
+        # check valid next layer type
+        assert(nextLayer == 'conv' or nextLayer == 'full')
+        # check valid pooling type
+        assert(type == 'max' or type == 'mean')
+        
+        self.numMaps = numMaps
         self.mapSize = mapSize
         self.winSize = winSize
+        self.nextLayer = nextLayer
         self.type = type
         self.lastOutput = None
         self.poolFunc = {'max': self._max_pool, 'mean': self._mean_pool}
         
     def forward_prop(self, maps):
+        # check same number of maps
+        assert(maps.shape[0] == self.numMaps)
         # check same map size
         assert(maps.shape[1] == self.mapSize)
         # check square maps
@@ -17,14 +28,13 @@ class PoolingLayer(Layer):
         # check integer number of pooling windows
         assert(maps.shape[1]%self.winSize == 0)
         
-        numMaps = maps.shape[0]
         poolW = self.mapSize/self.winSize
         
-        pooled = np.zeros((numMaps, poolW, poolW))
+        pooled = np.zeros((self.numMaps, poolW, poolW))
         if self.type == 'max':
-            pooledLoc = np.zeros((numMaps, poolW, poolW, 2))
+            pooledLoc = np.zeros((self.numMaps, poolW, poolW, 2))
         
-        for m in xrange(numMaps):
+        for m in xrange(self.numMaps):
             for i in xrange(poolW):
                 for j in xrange(poolW):
                     pool = self.poolFunc[self.type](maps[m, i*self.winSize:(i+1)*self.winSize, j*self.winSize:(j+1)*self.winSize])
@@ -38,12 +48,25 @@ class PoolingLayer(Layer):
         
         if self.type == 'max':          
             self.lastOutput = (pooled, pooledLoc)
-            return (pooled, pooledLoc)
+            if self.nextLayer == 'conv':
+                return (pooled, pooledLoc)
+            else:
+                return (pooled.flatten(), pooledLoc)
         else:
             self.lastOutput = pooled
-            return pooled
+            if self.nextLayer == 'conv':
+                return pooled
+            else:
+                return pooled.flatten()
+                
         
     def backward_prop(self, error):
+        if self.nextLayer == 'full':
+            # check can properly reshape
+            assert(error.shape[0] == self.numMaps*(self.mapSize/self.winSize)**2)
+        
+            error = error.reshape((self.numMaps, self.mapSize/self.winSize, self.mapSize/self.winSize))
+    
         # check square maps
         assert(error.shape[1] == error.shape[2])
         # check error size corresponds to winSize
@@ -55,6 +78,9 @@ class PoolingLayer(Layer):
         
         if self.type == 'max':
             (pooled, pooledLoc) = self.lastOutput
+            
+            if self.nextLayer == 'full':
+                pooled = pooled.reshape((pooledLoc.shape[0], pooledLoc.shape[1], pooledLoc.shape[2]))
             
             for m in xrange(pooledLoc.shape[0]):
                 for i in xrange(pooledLoc.shape[1]):
