32a3fc7d61b9d798fc185d63c60a71e1ea16bcd7
Julian Gonzalez <jugonz97@mit.edu>
2014-12-04 00:28:28 -0500

    added misc functions for image manipulation

---
+++ misc.py
@@ -0,0 +1,18 @@
+import numpy as np
+import scipy.misc as smisc
+import scipy.io as sio
+
+def loadImage(filePath):
+    return smisc.imread(filePath)
+
+def saveImage(image, filename):
+	smisc.imsave(filename, image)
+
+# Save a dictionary of names and np arrays
+# to a .mat file in the current folder.
+def saveToMatlab(filename, dictionary):
+	sio.savemat(filename, dictionary)
+
+# Return a dictionary of matlab data.
+def readFromMatlab(filename):
+	return sio.loadmat(filename)
