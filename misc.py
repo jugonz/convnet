52971eb02e93d13ea1d59bddf50e2716e6d5e014
Julian Gonzalez <jugonz97@mit.edu>
2014-12-04 16:47:37 -0500

    convert misc to use spaces

--- misc.py
+++ misc.py
@@ -6,13 +6,13 @@ def loadImage(filePath):
     return smisc.imread(filePath)
 
 def saveImage(image, filename):
-	smisc.imsave(filename, image)
+    smisc.imsave(filename, image)
 
 # Save a dictionary of names and np arrays
 # to a .mat file in the current folder.
 def saveToMatlab(filename, dictionary):
-	sio.savemat(filename, dictionary)
+    sio.savemat(filename, dictionary)
 
 # Return a dictionary of matlab data.
 def readFromMatlab(filename):
-	return sio.loadmat(filename)
+    return sio.loadmat(filename)
