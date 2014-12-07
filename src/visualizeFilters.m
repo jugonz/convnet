c673de007b63320ef889eb91355d2b6d46e8a3f3
Julian Gonzalez <jugonz97@mit.edu>
2014-12-07 17:39:23 -0500

    Added gain to filter visualization.
    This helps dark filters become more visible.

--- src/visualizeFilters.m
+++ src/visualizeFilters.m
@@ -9,18 +9,33 @@ function visualizeFilters(file, outputName)
 % outputName is the prefix of the image files to be output.
 borderWidth = 2;
 magnifyRatio = 1; % Usually we need to upscale the output to be visible.
+gain = 25/255; % How much to increase pixel values by.
 
 data = load(file, 'numLayers', 'filters');
 numLayers = data.numLayers;
 filters = data.filters;
 assert(numLayers > 0, 'No layers to process!');
-visualizeFiltersNoMAT(numLayers, filters, borderWidth, magnifyRatio, outputName);
+visualizeFiltersNoMAT(numLayers, filters, borderWidth, magnifyRatio, gain, outputName);
 end
 
-function visualizeFiltersNoMAT(numLayers, filters, borderWidth, magnifyRatio, outputName)
+function visualizeFiltersNoMAT(numLayers, filters, borderWidth, magnifyRatio, gain, outputName)
 % For each layer...
 for i=1:numLayers
     layer = filters{i};
+
+    % Add the gain to each value in the image, clamping if needed.
+    for j=1:size(layer, 1)
+        for k=1:size(layer, 2)
+            for l=1:size(layer, 3)
+                gained = layer(j, k, l) + gain;
+                if gained >= 1
+                    gained = 1.;
+                end
+                layer(j, k, l) = gained;
+            end
+        end
+    end
+
     % Resize the image for better visibility.
     layerResized = zeros(size(layer, 1), magnifyRatio * size(layer, 2), ...
         magnifyRatio * size(layer, 3));
@@ -30,13 +45,13 @@ for i=1:numLayers
         layerResized(j, :, :) = resized;
     end
     layer = layerResized;
-    
+
     % Keep track of other paramaters.
     numFilters = size(layer, 1);
     filterDim = size(layer, 2);
     border = zeros(filterDim, borderWidth, 3); % 3 for RGB
     border(:, :, 3) = ones(filterDim, borderWidth);
-    
+
     % For each filter...
     % add to our final image plus a border.
     newIm = [];
@@ -47,7 +62,7 @@ for i=1:numLayers
             newIm = [newIm border];
         end
     end
-    
+
     % Save the image to PNG.
     imwrite(newIm, strcat(outputName, '_layer_', num2str(i), '.png'));
 end
