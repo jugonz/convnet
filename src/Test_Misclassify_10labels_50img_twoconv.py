69189f37615dd0d54061360cc8e47aa11be23e9a
Julian Gonzalez <jugonz97@mit.edu>
2014-12-12 19:16:55 -0500

    added abstract and reformatted

--- src/Test_Misclassify_10labels_50img_twoconv.py
+++ src/Test_Misclassify_10labels_50img_twoconv.py
@@ -7,15 +7,14 @@ import misc
 
 class MNISTMisclassifyTest(unittest.TestCase):
     def setUp(self):
-        configPath = "../10labels_50img_twoconv_trained/MNIST10labels_50img_twoConv-trained-50.ini"
+        configPath = "../ini/MNIST10labels_50img_twoConv-trained-50.ini"
         self.net = ConvNet(configPath)
 
-        # import just images of 2s and 3s for now
         (self.images, self.labels) = read_mnist.getMNISTTest()
-		
+
     def testTest10Labels(self):
         str_labels = []
-        
+
         for digit in self.labels:
             if digit == 0:
                 str_labels.append("zero")
@@ -37,9 +36,9 @@ class MNISTMisclassifyTest(unittest.TestCase):
                 str_labels.append("eight")
             elif digit == 9:
 	            str_labels.append("nine")
-        
+
         numWrong = 10
-        
+
 		# run test
         for i in xrange(len(self.images)):
             # Propagate our sample through the network.
@@ -54,8 +53,8 @@ class MNISTMisclassifyTest(unittest.TestCase):
             secondLargest = outputVector[0].tolist().index(sortedOutputVector[1])
 
             # Save the number of correct labels.
-            if outputLabel != expectedOutputLabel:
-                misc.saveImage(self.images[i], "misclass_"+str(expectedOutputLabel)+"_"+str(largest)+"_"+str(secondLargest)+".png")
+            if outputLabel == expectedOutputLabel:
+                misc.saveImage(self.images[i], "twoconv_corclass/" + "corclass_"+str(expectedOutputLabel)+"_"+str(largest)+"_"+str(secondLargest)+".png")
 
 if __name__ == "__main__":
     unittest.main()
